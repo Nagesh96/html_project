68b2c3da5c54570bd5ecf1ebe7da60e1d57d7c7c
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-07 20:28:53 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -1,4 +1,19 @@
-def LOG_FILE = env.BUILD_LOG_ENV
-def command = "grep '\\[INFO\\] Deploying artifact:' $LOG_FILE | awk '{print \$NF}'"
-def url = sh(script: command, returnStdout: true).trim()
-echo "Extracted URL: $url"
+pipeline {
+    agent any
+
+    stages {
+        stage('Search Log File') {
+            steps {
+                script {
+                    def logFilePath = 'path/to/your/logfile.log'
+                    def pattern = 'Deploying artifact: (http[s]?:\/\/(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,7}(?::\d{1,5})?(?:\/\S*)?\/(?:\S+\/)*\S+\.(?:jar|war))'
+
+                    def grepCommand = "grep -E \"$pattern\" $logFilePath"
+                    def grepOutput = sh(script: grepCommand, returnStdout: true).trim()
+
+                    echo "Grep Output:\n$grepOutput"
+                }
+            }
+        }
+    }
+}

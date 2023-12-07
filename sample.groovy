76ab962eff4007864a9228d64be840c2dbbf003f
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-07 20:32:41 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -6,7 +6,7 @@ pipeline {
             steps {
                 script {
                     def logFilePath = 'path/to/your/logfile.log'
-                    def pattern = 'Deploying artifact: (http[s]?:\/\/(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,7}(?::\d{1,5})?(?:\/\S*)?\/(?:\S+\/)*\S+\.(?:jar|war))'
+                    def pattern = 'Deploying artifact: (http[s]?:\\/\\/(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}(?::\\d{1,5})?(?:\\/\\S*)?\\/(?:\\S+\\/)*\\S+\\.(?:jar|war))'
 
                     def grepCommand = "grep -E \"$pattern\" $logFilePath"
                     def grepOutput = sh(script: grepCommand, returnStdout: true).trim()

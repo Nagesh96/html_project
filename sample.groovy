46dc534f4507eae812af70e26ad6c1d1279893bf
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-07 16:20:02 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -1,6 +1,4 @@
-def consoleOutput = '[consumer_e] Deploying artifact: http://54.211.105.217:8082/artifactory/example-repo-local/addressbook.war'
-def pattern = /http:\/\/[\d.:\/]+addressbook\.war/
-def url = (consoleOutput =~ pattern).find { it }
-if (url) {
-    println "Found URL: ${url[0]}"
-}
+def LOG_FILE = env.BUILD_LOG_ENV
+def command = "grep '\\[INFO\\] Deploying artifact:' $LOG_FILE | awk '{print \$NF}'"
+def url = sh(script: command, returnStdout: true).trim()
+echo "Extracted URL: $url"

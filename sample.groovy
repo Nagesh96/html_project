20374a92c9177b0d601fc9262069e9da437f9657
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-08 16:02:12 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -88,3 +88,13 @@ stage('Search Log File') {
 
 
 https://jfrog.charter.com/artifactory/mobile-snapshot-local/com/spectrum/mobile/mbo-dataassetbuildup/4.0.0-SNAPSHOT/mbo-dataassetbuildup-4.0.0-SNAPSHOT.war
+
+
+
+
+https://jfrog\.charter\.com/artifactory/(.+\/)*([^\/]+\.(jar|war))
+
+
+
+
+        

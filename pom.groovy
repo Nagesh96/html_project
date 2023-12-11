930e519d8433a460038f4646aafe411cb2a07fc2
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-11 18:37:09 +0530

    Update pom.groovy

--- pom.groovy
+++ pom.groovy
@@ -31,9 +31,21 @@ pipeline {
 }
 
 
-def pom = readMavenPom file: 'pom.xml'
 
-def ARTIFACT_ID = pom.artifactId
-def version = pom.version
-
-sh "echo 'VERSION=${version != null ? version.replaceAll('-null', '') : ''}'"
+                    def pom = readMavenPom file: 'pom.xml'
+                    def ARTIFACT_ID = pom.artifactId
+                    def version = pom.version
+                    sh "echo version=${version} | sed 's/-null//' "
+                    echo "Artifact ID: ${ARTIFACT_ID}"
+                    env.VERSION = version
+                    env.ARTIFACT_ID = ARTIFACT_ID
+                    def FIELD_ID = "customfield_17856" 
+                    env.FIELD_ID = FIELD_ID
+                    def JFROG_ARTIFACTURL = "https://jfrog.charter.com/ui/builds/mobile-backend-builds%20::%20qualitygates-mobilecicd%20::%20${JOB_BASE_NAME}/${BUILD_NUMBER}"
+                    VALUE = ${ARTIFACT_ID}-${VERSION}-${BUILD_NUMBER}
+                    env.VALUE = VALUE
+                    def text = "Reference URL of the published Artifact: "
+                    def link = "${JFROG_ARTIFACTURL}"
+                    COMMENT = "${text}${link}"
+                    env.COMMENT = COMMENT
+                    VERSION=4.0.0-02-${BUILD_NUMBER}

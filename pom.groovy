085d795d738f6d98857fbd616acf3197dd605b68
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-11 17:03:23 +0530

    Update pom.groovy

--- pom.groovy
+++ pom.groovy
@@ -4,3 +4,28 @@ def version = pom.version
 
 echo "Artifact ID: ${artifactId}"
 echo "Version: ${version}"
+
+
+
+pipeline {
+    agent any
+
+    parameters {
+        gitParameter branchFilter: '.*', defaultValue: 'master', description: 'Select the branch', name: 'BRANCH'
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                script {
+                    // Retrieve the selected branch from the parameter
+                    def selectedBranch = params.BRANCH ?: 'master'
+
+                    // Checkout the selected branch
+                    git branch: selectedBranch, url: 'https://github.com/your/repo.git'
+                }
+            }
+        }
+        // Other stages in your pipeline
+    }
+}
